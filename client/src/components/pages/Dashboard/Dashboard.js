02c4ad2fd8b5b8c84b8b51faeefd8bc937d9ab5c
Grant Chen <grantchen2021@gmail.com>
2022-07-17 13:27:23 -0400

    removed all dashboard components

--- client/src/components/pages/Dashboard/Dashboard.js
+++ client/src/components/pages/Dashboard/Dashboard.js
@@ -1,21 +1,9 @@
-import { Box } from "@mui/system";
-import React from "react";
-import { Route, Routes } from "react-router-dom";
-import UnavailableResource from "../UnavailableResource/UnavailableResource";
-import DashboardHome from "./DashboardHome/DashboardHome";
-import DashboardStats from "./DashboardStats/DashboardStats";
-import Wrapper from "./Wrapper";
+import React from 'react'
 
 const Dashboard = () => {
   return (
-    <Routes>
-      <Route element={<Wrapper />}>
-        <Route path="/" element={<DashboardHome />} />
-        <Route path="/stats" element={<DashboardStats />} />
-      </Route>
-      <Route path="*" element={<UnavailableResource />} />
-    </Routes>
-  );
-};
+    <div>Dashboard</div>
+  )
+}
 
-export default Dashboard;
+export default Dashboard

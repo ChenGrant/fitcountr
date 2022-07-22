dbc082fc639e93b8ecb1f33f742ee171f5fb85fc
Grant Chen <grantchen2021@gmail.com>
2022-07-22 01:29:48 -0400

    idek

--- server/src/controllers/searchFoodController.js
+++ server/src/controllers/searchFoodController.js
@@ -1,4 +1,9 @@
 const fs = require("fs");
+var CloudmersiveBarcodeapiClient = require("cloudmersive-barcodeapi-client");
+const defaultClient = CloudmersiveBarcodeapiClient.ApiClient.instance;
+const Apikey = defaultClient.authentications["Apikey"];
+Apikey.apiKey = "ed1bc5ed-4eca-4352-a9ba-99ce00116bd2";
+
 const postBarcode = (req, res) => {
   const newpath = __dirname + "\\public\\";
   console.log(newpath);
@@ -10,11 +15,26 @@ const postBarcode = (req, res) => {
   // Use the mv() method to place the file somewhere on your server
   sampleFile.mv(uploadPath, function (err) {
     if (err) {
-      console.log(err)
-      return res.status(500).send(err);
+      console.log(err);
+      return res.status(500).json({ error: "true" });
     }
 
-    res.send("File uploaded!");
+    console.log("made it");
+
+    var apiInstance = new CloudmersiveBarcodeapiClient.BarcodeScanApi();
+
+    var imageFile = Buffer.from(fs.readFileSync(uploadPath).buffer); // File | Image file to perform the operation on.  Common file formats such as PNG, JPEG are supported.
+
+    var callback = function (error, data, response) {
+      if (error) {
+        console.error(error);
+      } else {
+        console.log("API called successfully. Returned data: ", data);
+      }
+
+      res.json(data);
+    };
+    apiInstance.barcodeScanImage(imageFile, callback);
   });
 };
 

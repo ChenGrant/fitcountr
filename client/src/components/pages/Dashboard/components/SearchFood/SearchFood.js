6511c325a6d22561406f229f0b65000fc27147ea
Grant Chen <grantchen2021@gmail.com>
2022-07-22 14:42:22 -0400

    displayed scanned barcode and made it so user can only upload 1 barcode at a time

--- client/src/components/pages/Dashboard/components/SearchFood/SearchFood.js
+++ client/src/components/pages/Dashboard/components/SearchFood/SearchFood.js
@@ -2,26 +2,39 @@ import { Box, TextField, Typography } from "@mui/material";
 import React, { useState } from "react";
 import { fetchBarcode } from "../../../../../utils/fetchRequestUtils";
 import CustomButton from "../../../../ui/CustomButton";
+import LoadingCircle from "../../../../ui/LoadingCircle";
 
 const GAP_SIZE = 4;
 
 const SearchFood = () => {
   const [files, setFiles] = useState();
+  const [barcode, setBarcode] = useState("");
+  const [scanning, setScanning] = useState(false);
 
   const handleSubmit = async () => {
     if (files === undefined || files.length === 0) return;
-    console.log(files[0])
+    setScanning(true);
+    console.log(files[0]);
     const fetchedBarcode = await fetchBarcode(files[0]);
+    setBarcode(fetchedBarcode.RawText);
     console.log(fetchedBarcode);
+    setScanning(false);
   };
 
   return (
     <Box p={4} display="flex" flexDirection="column" gap={GAP_SIZE}>
-      <Typography>Upload Image of a Food's Barcode</Typography>
-      <TextField type="file" onChange={(e) => setFiles(e.target.files)} />
-      <CustomButton variant="contained" onClick={handleSubmit}>
-        Submit Barcode
-      </CustomButton>
+      {scanning ? (
+        <LoadingCircle />
+      ) : (
+        <>
+          <Typography>Upload Image of a Food's Barcode</Typography>
+          <TextField type="file" onChange={(e) => setFiles(e.target.files)} />
+          <CustomButton variant="contained" onClick={handleSubmit}>
+            Submit Barcode
+          </CustomButton>
+        </>
+      )}
+      {barcode && <Typography>Barcode: {barcode}</Typography>}
     </Box>
   );
 };

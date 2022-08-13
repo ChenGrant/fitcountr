00c5a5c4ed54301e56d31c35a981add6267016d9
Grant Chen <grantchen2021@gmail.com>
2022-08-13 13:29:36 -0400

    added progress page title, tabs, goal edit and add new stat

--- client/src/pages/Progress/components/Progress.js
+++ client/src/pages/Progress/components/Progress.js
@@ -1,16 +1,23 @@
-import { Typography } from "@mui/material";
+import { IconButton, Tab, Tabs, Typography } from "@mui/material";
 import { Box } from "@mui/system";
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import LoadingCircle from "../../../components/miscellaneous/LoadingCircle";
+import CustomButton from "../../../components/ui/CustomButton";
 import { setProgressPageStat } from "../../../redux";
-import { getLexSmallest, PROGRESS_TYPES } from "../../../utils";
-
+import {
+  capitalizeFirstCharacterLowercaseRest,
+  getLexSmallest,
+  PROGRESS_TYPES,
+  sortArray,
+} from "../../../utils";
+import EditIcon from "@mui/icons-material/Edit";
+import AddIcon from "@mui/icons-material/Add";
 const Progress = () => {
   const { progressPage } = useSelector((state) => state);
   const dispatch = useDispatch();
 
-  const pageIsLoading = progressPage === null;
+  const pageIsLoading = progressPage.stat === null;
 
   useEffect(() => {
     if (progressPage.stat === null) {
@@ -23,9 +30,58 @@ const Progress = () => {
   if (pageIsLoading) return <LoadingCircle />;
 
   return (
-    <Box>
-      <Typography>Progress</Typography>
-      <Typography>{progressPage.stat}</Typography>
+    <Box p={5} display="flex" flexDirection="column" gap={4}>
+      <Typography variant="h1">Progress</Typography>
+      <Box display="flex" alignItems="center" gap={15}>
+        <Tabs
+          value={progressPage.stat}
+          onChange={(e, newProgressType) =>
+            dispatch(setProgressPageStat(newProgressType))
+          }
+        >
+          {sortArray(
+            Object.values(PROGRESS_TYPES),
+            (progressType1, progressType2) =>
+              progressType1.localeCompare(progressType2)
+          ).map((progressType) => {
+            return (
+              <Tab
+                sx={{
+                  textTransform: "none",
+                  fontSize: "220px",
+                  color: "black",
+                }}
+                key={progressType}
+                value={progressType}
+                label={
+                  <span style={{ fontSize: "20px" }}>
+                    {capitalizeFirstCharacterLowercaseRest(progressType)}
+                  </span>
+                }
+              />
+            );
+          })}
+        </Tabs>
+        <Box display="flex" gap={1} alignItems="center">
+          <Typography variant="h6">Goal: {45.7} kg</Typography>
+          <IconButton>
+            <EditIcon />
+          </IconButton>
+        </Box>
+        <Box>
+          <CustomButton variant="contained">
+            <Box
+              display="flex"
+              alignItems="center"
+              justifyContent="center"
+              gap={1}
+            >
+              <AddIcon />
+              <Typography variant="h6">New Weight</Typography>
+            </Box>
+          </CustomButton>
+        </Box>
+      </Box>
     </Box>
   );
 };

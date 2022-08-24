d76ba0bcc6acc3e96d26b3f4ab417fca18009c4e
Grant Chen <grantchen2021@gmail.com>
2022-08-24 03:42:44 -0400

    fixed pagination number upon user.progress change

--- client/src/pages/Progress/components/ProgressTable/ProgressTable.js
+++ client/src/pages/Progress/components/ProgressTable/ProgressTable.js
@@ -3,11 +3,13 @@ import { Box, Pagination, Typography } from "@mui/material";
 import React, { useEffect, useReducer } from "react";
 import { useSelector } from "react-redux";
 import CustomCard from "../../../../components/ui/CustomCard";
+import { PROGRESS_TYPE_NAMES } from "../../../../utils";
 import { getColumnsHeaders, getRows } from "../../utils";
 import ProgressTableRow from "./ProgressTableRow";
 
 // ------------------------------------ CONSTANTS ------------------------------------
-const PROGRESS_ITEMS_PER_PAGE = 5;
+const PROGRESS_ITEMS_PER_PAGE = 7;
+const INITIAL_PAGE_NUMBER = 1;
 
 // ---------------------------------- TABLE REDUCER ----------------------------------
 const TABLE_ACTIONS = {
@@ -19,7 +21,7 @@ const TABLE_ACTIONS = {
 const INITIAL_TABLE_STATE = {
   columnHeaders: null,
   rows: null,
-  pageNumber: 1,
+  pageNumber: null,
 };
 
 const tableReducer = (state, action) => {
@@ -44,6 +46,11 @@ const ProgressTable = () => {
   const [table, tableDispatch] = useReducer(tableReducer, INITIAL_TABLE_STATE);
   const theme = useTheme();
 
+  const displayableProgressItems = table.rows?.slice(
+    PROGRESS_ITEMS_PER_PAGE * (table.pageNumber - 1),
+    PROGRESS_ITEMS_PER_PAGE * table.pageNumber
+  );
+
   // ----------------------------------- USE EFFECT -----------------------------------
   useEffect(() => {
     const columnsHeaders = getColumnsHeaders(progressType, user.goals);
@@ -56,6 +63,25 @@ const ProgressTable = () => {
       payload: getRows(progressType, user.progress, columnsHeaders),
     });
   }, [progressType, user.progress, user.goals]);
+
+  // reset page number back to INITIAL_PAGE_NUMBER on progressType change
+  useEffect(() => {
+    tableDispatch({
+      type: TABLE_ACTIONS.SET_PAGE_NUMBER,
+      payload: INITIAL_PAGE_NUMBER,
+    });
+  }, [progressType]);
+
+  // when user.progress changes, make sure the page number is
+  // not out of bounds
+  useEffect(() => {
+    if (displayableProgressItems?.length === 0 && table.pageNumber > 1) {
+      tableDispatch({
+        type: TABLE_ACTIONS.SET_PAGE_NUMBER,
+        payload: table.pageNumber - 1,
+      });
+    }
+  }, [displayableProgressItems, table.pageNumber]);
 
   // ------------------------------------- RENDER -------------------------------------
   return (
@@ -76,18 +102,22 @@ const ProgressTable = () => {
           ))}
         </Box>
         {/* Table Rows */}
-        {table.rows
-          ?.slice(
-            PROGRESS_ITEMS_PER_PAGE * (table.pageNumber - 1),
-            PROGRESS_ITEMS_PER_PAGE * table.pageNumber
-          )
-          .map((row) => (
+        {displayableProgressItems?.length === 0 ? (
+          <Box borderTop="1px solid #D3D3D3" py={2} px={1}>
+            <Typography textAlign="center">
+              No progress. Click the button above to add new{" "}
+              {PROGRESS_TYPE_NAMES[progressType].plural}.
+            </Typography>
+          </Box>
+        ) : (
+          displayableProgressItems?.map((row) => (
             <ProgressTableRow
               key={row.id}
               row={row}
               columnHeaders={table.columnHeaders}
             />
-          ))}
+          ))
+        )}
         {/* Pagination */}
         {table.rows && (
           <Box display="grid" sx={{ placeItems: "center", mt: 3 }}>

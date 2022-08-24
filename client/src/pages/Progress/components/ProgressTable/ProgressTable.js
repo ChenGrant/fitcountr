e9fe258b037ce02a1b6dcbd338890a24b2a6187a
Grant Chen <grantchen2021@gmail.com>
2022-08-24 03:10:29 -0400

    added pagination for ProgressTable

--- client/src/pages/Progress/components/ProgressTable/ProgressTable.js
+++ client/src/pages/Progress/components/ProgressTable/ProgressTable.js
@@ -1,19 +1,25 @@
-import { Box, Typography } from "@mui/material";
+import { useTheme } from "@emotion/react";
+import { Box, Pagination, Typography } from "@mui/material";
 import React, { useEffect, useReducer } from "react";
 import { useSelector } from "react-redux";
 import CustomCard from "../../../../components/ui/CustomCard";
 import { getColumnsHeaders, getRows } from "../../utils";
 import ProgressTableRow from "./ProgressTableRow";
 
+// ------------------------------------ CONSTANTS ------------------------------------
+const PROGRESS_ITEMS_PER_PAGE = 5;
+
 // ---------------------------------- TABLE REDUCER ----------------------------------
 const TABLE_ACTIONS = {
   SET_COLUMN_HEADERS: "SET_COLUMN_HEADERS",
   SET_ROWS: "SET_ROWS",
+  SET_PAGE_NUMBER: "SET_PAGE_NUMBER",
 };
 
 const INITIAL_TABLE_STATE = {
   columnHeaders: null,
   rows: null,
+  pageNumber: 1,
 };
 
 const tableReducer = (state, action) => {
@@ -22,6 +28,8 @@ const tableReducer = (state, action) => {
       return { ...state, columnHeaders: action.payload };
     case TABLE_ACTIONS.SET_ROWS:
       return { ...state, rows: action.payload };
+    case TABLE_ACTIONS.SET_PAGE_NUMBER:
+      return { ...state, pageNumber: action.payload };
     default:
       return state;
   }
@@ -34,6 +42,7 @@ const ProgressTable = () => {
   const { progressPage, user } = useSelector((state) => state);
   const { progressType } = progressPage;
   const [table, tableDispatch] = useReducer(tableReducer, INITIAL_TABLE_STATE);
+  const theme = useTheme();
 
   // ----------------------------------- USE EFFECT -----------------------------------
   useEffect(() => {
@@ -67,13 +76,46 @@ const ProgressTable = () => {
           ))}
         </Box>
         {/* Table Rows */}
-        {table.rows?.map((row) => (
-          <ProgressTableRow
-            key={row.id}
-            row={row}
-            columnHeaders={table.columnHeaders}
-          />
-        ))}
+        {table.rows
+          ?.slice(
+            PROGRESS_ITEMS_PER_PAGE * (table.pageNumber - 1),
+            PROGRESS_ITEMS_PER_PAGE * table.pageNumber
+          )
+          .map((row) => (
+            <ProgressTableRow
+              key={row.id}
+              row={row}
+              columnHeaders={table.columnHeaders}
+            />
+          ))}
+        {/* Pagination */}
+        {table.rows && (
+          <Box display="grid" sx={{ placeItems: "center", mt: 3 }}>
+            <Pagination
+              count={Math.ceil(table.rows.length / PROGRESS_ITEMS_PER_PAGE)}
+              page={table.pageNumber}
+              onChange={(e, pageNumber) =>
+                tableDispatch({
+                  type: TABLE_ACTIONS.SET_PAGE_NUMBER,
+                  payload: pageNumber,
+                })
+              }
+              shape="rounded"
+              color="primary"
+              sx={{
+                "& .MuiPaginationItem-root": {
+                  color: "black",
+                  "&:hover": {
+                    background: theme.palette.primary.light,
+                  },
+                  "&.Mui-selected": {
+                    background: theme.palette.primary.main,
+                  },
+                },
+              }}
+            />
+          </Box>
+        )}
       </CustomCard>
     </Box>
   );
